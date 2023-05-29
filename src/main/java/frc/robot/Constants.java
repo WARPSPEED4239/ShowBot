45fc98d64869cb7f6c38763ccafe57a76aa9fa30
Darren Seubert <dpseubert15@gmail.com>
2023-05-29 08:40:19 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,7 @@ public final class Constants {
 
     public static final double EPSILON = 0.0001,
                                ROTATION_SPEED = 0.45,
-                               MAX_ROTATION_SPEED = 0.7,
+                               MAX_ROTATION_SPEED = 0.75,
                                MIN_FIRING_PRESSURE_INSIDE = 80.0,  // TODO Adjust Inside and Outside Pressures
                                MAX_FIRING_PRESSURE_INSIDE = 87.0,
                                MIN_FIRING_PRESSURE_OUTSIDE = 110.0,
