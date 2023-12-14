2bc3ddbd4b132c6d3da9ed752f6aaf06c601a4cb
Darren Seubert <dpseubert15@gmail.com>
2023-12-14 17:07:11 -0600

    Added in RGB Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,16 +2,17 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.tools.RGBController.Color;
 
 public class Robot extends TimedRobot {
-  // private RobotContainer mRobotContainer;
+  private RobotContainer mRobotContainer;
 
   @Override
   public void robotInit() {
-    /*mRobotContainer =*/ new RobotContainer();
+    mRobotContainer = new RobotContainer();
 
-    // Color[] colors = {Color.RedDim, Color.Black};
-    // mRobotContainer.getRGBController().setColors(colors, 2.0);
+    Color[] colors = {Color.RedDim, Color.Black};
+    mRobotContainer.getRGBController().setColors(colors, 2.0);
   }
 
   @Override
@@ -21,8 +22,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledInit() {
-    // Color[] colors = {Color.RedDim, Color.Black};
-    // mRobotContainer.getRGBController().setColors(colors, 2.0);
+    Color[] colors = {Color.RedDim, Color.Black};
+    mRobotContainer.getRGBController().setColors(colors, 2.0);
   }
 
   @Override
